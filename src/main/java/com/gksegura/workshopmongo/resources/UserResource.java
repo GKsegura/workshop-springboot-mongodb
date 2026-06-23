4fd3529d6b3e4d0fdd84abf62c3adad965760df1
José Segura <jose.segura.ms@gmail.com>
2026-06-22 23:54:55 -0300

    Insercao de usuario com POST

--- src/main/java/com/gksegura/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gksegura/workshopmongo/resources/UserResource.java
@@ -1,17 +1,16 @@
 package com.gksegura.workshopmongo.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gksegura.workshopmongo.domain.User;
 import com.gksegura.workshopmongo.dto.UserDTO;
 import com.gksegura.workshopmongo.services.UserService;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,5 +30,13 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id) {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(new UserDTO(obj));
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+        User obj = service.fromDTO(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/gksegura/workshopmongo/services/UserService.java
+++ src/main/java/com/gksegura/workshopmongo/services/UserService.java
@@ -3,6 +3,7 @@ package com.gksegura.workshopmongo.services;
 import java.util.List;
 import java.util.Optional;
 
+import com.gksegura.workshopmongo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
